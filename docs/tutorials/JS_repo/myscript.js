7c3146c6ae4cf96016ef6123557eb916e33ec369
ah3918 <ah3918@ic.ac.uk>
2023-02-24 11:02:53 +0000

    add js script

---
+++ docs/tutorials/JS_repo/myscript.js
@@ -0,0 +1,10 @@
+// myscript.js
+
+// Get a reference to the "About Me" heading
+var aboutMeHeading = document.querySelector('h2');
+
+// Add a click event listener to the "About Me" heading
+aboutMeHeading.addEventListener('click', function() {
+  // Display an alert message when the heading is clicked
+  alert('You clicked on the "About Me" heading!');
+});
