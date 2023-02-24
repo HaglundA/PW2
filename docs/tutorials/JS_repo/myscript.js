792c6860bad9476d6ea4041c77f174479dd6f01a
ah3918 <ah3918@ic.ac.uk>
2023-02-24 11:19:52 +0000

    test

--- docs/tutorials/JS_repo/myscript.js
+++ docs/tutorials/JS_repo/myscript.js
@@ -1,14 +1,15 @@
-// myscript.js
-
-const tutorialsButton = document.getElementById("tutorials-button");
-const tutorialList = document.getElementById("tutorial-list");
-
-tutorialsButton.addEventListener("click", function() {
-  if (tutorialList.style.display === "none") {
-    tutorialList.style.display = "block";
-    document.body.style.marginTop = tutorialList.offsetHeight + "px";
-  } else {
-    tutorialList.style.display = "none";
-    document.body.style.marginTop = "0px";
+// Toggle tutorial list when "Tutorials" button is clicked
+function toggleTutorialList() {
+    var tutorialList = document.getElementById("tutorial-list");
+    if (tutorialList.style.display === "none") {
+      tutorialList.style.display = "block";
+      document.body.style.marginTop = "120px";
+    } else {
+      tutorialList.style.display = "none";
+      document.body.style.marginTop = "0";
+    }
   }
-});
+  
+  var tutorialsButton = document.getElementById("tutorials-button");
+  tutorialsButton.addEventListener("click", toggleTutorialList);
+  
