5201aa80149cf25519797a42fc8e2c6b2417da47
ah3918 <ah3918@ic.ac.uk>
2023-02-24 11:46:23 +0000

    -test3

--- docs/JS_repo/myscript.js
+++ docs/JS_repo/myscript.js
@@ -1,14 +1,12 @@
-const tutorialsButton = document.getElementById("tutorials-button");
-const tutorialList = document.getElementById("tutorial-list");
+var tutorialsButton = document.getElementById("tutorials-button");
+var tutorialList = document.getElementById("tutorial-list");
 
-function toggle() {
+tutorialsButton.addEventListener("click", function() {
   if (tutorialList.style.display === "none") {
     tutorialList.style.display = "block";
-    document.body.style.marginTop = tutorialList.offsetHeight + "px";
+    document.body.style.marginTop = "200px"; // or whatever height you want to shift the page down by
   } else {
     tutorialList.style.display = "none";
     document.body.style.marginTop = "0";
   }
-}
-
-tutorialsButton.addEventListener("click", toggle);
+});
